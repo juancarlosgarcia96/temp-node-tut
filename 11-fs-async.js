0ad3011ce6b42b2b08ed622862b6e559480c912d
juancarlosgarcia96 <78667978+juancarlosgarcia96@users.noreply.github.com>
2021-10-21 13:55:39 +0200

    first commit

---
+++ 11-fs-async.js
@@ -0,0 +1,36 @@
+const {readFile, writeFile} = require('fs');
+console.log("start")
+//callback lo runeamos cuando ya hemos acabado la funcionalidad(completada)
+
+//ponemos el encoding para que no nos devuelva el buffer
+readFile('./content/first.txt','utf8',(err,result)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    const first = result;
+    readFile('./content/second.txt','utf-8',(err,result)=>{
+
+        if(err){
+            console.log(err);
+            return;
+        }
+    const second = result;
+    writeFile('./content/result-async.txt',
+    `Here is the result: ${first}, ${second}`,
+    (err,result)=>{
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log('done with this task');
+
+        
+    })
+    
+    })
+
+
+    })
+    console.log('starting next task')
+
---
+++ 4-names.js
@@ -0,0 +1,10 @@
+// local
+const secret = 'SUPER SECRET'
+// share
+const jhon = 'jhon'
+const peter = 'peter'
+
+//console.log(module);
+
+//lo igualamos a un objeto
+module.exports = { jhon , peter } 
